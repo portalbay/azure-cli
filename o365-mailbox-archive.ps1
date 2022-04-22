3cb4dae68394e74d57c351c0b3afd0891b055fd0
scv_ready <74455344+portalbay@users.noreply.github.com>
2022-04-22 04:52:01 -0400

    Create o365-mailbox-archive.ps1

---
+++ o365-mailbox-archive.ps1
@@ -0,0 +1,13 @@
+# https://docs.microsoft.com/en-us/microsoft-365/compliance/enable-autoexpanding-archiving?view=o365-worldwide 
+
+#Mailbox Archive 
+
+        #Enablee for specific user 
+
+        # Get-Mailbox <user mailbox> | FL AutoExpandingArchiveEnabled 
+
+        # Enable-Mailbox <user mailbox> -AutoExpandingArchive 
+
+        # Get-Mailbox <user mailbox> | FL AutoExpandingArchiveEnabled 
+
+# 
